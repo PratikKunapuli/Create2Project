36e56e0de8a956d577b1788cf17979f96cc4540c
Pratik Kunapuli <pak62897@gmail.com>
2018-01-31 12:14:01 -0500

    Edited print statments to be compliant with python3, added DEBUG flag for command line input

--- runRobot.py
+++ runRobot.py
@@ -1,33 +1,43 @@
 #import create2api
 import os
+import sys
 import controller
 
 
-#bot = create2api.Create2()
-joystick = controller.PS4Controller()
-joystick.init()
-#joystick.listen()
-
-exitButton = False #X on the PS4 controller.
-while(not exitButton):
-    joystick.pollEvents()
-    exitButton = joystick.getXButton()
-    leftX = joystick.getLeftXAxis()
-    leftY = joystick.getLeftYAxis()
-    rightX = joystick.getRightXAxis()
-    rightY = joystick.getRightYAxis()
-
-    print('LeftX= {:f}'.format(leftX))
-    print('LeftY= {:f}'.format(leftY))
-    print('RightX= {:f}'.format(rightX))
-    print('RightY= {:f}'.format(rightY))
+if __name__ == "__main__":
+    """
+        Initialization
+    """
+    if(len(sys.argv) > 1):
+        DEBUG = sys.argv[1] == "DEBUG"
 
 
-    os.system('clear')
-    #print("I got here!")
+    #bot = create2api.Create2()
+    #bot.start()
+    #bot.safe()
+    joystick = controller.PS4Controller()
+    joystick.init()
+    if DEBUG:
+        joystick.listen()
 
-#bot.start()
-#bot.safe()
+    exitButton = False #X on the PS4 controller.
 
+    """
+        Looping Code
+    """
+    while(not exitButton):
+        joystick.pollEvents()
+        exitButton = joystick.getXButton()
+        leftX = joystick.getLeftXAxis()
+        leftY = joystick.getLeftYAxis()
+        rightX = joystick.getRightXAxis()
+        rightY = joystick.getRightYAxis()
 
+        if DEBUG:
+            print('LeftX= {:f}'.format(leftX))
+            print('LeftY= {:f}'.format(leftY))
+            print('RightX= {:f}'.format(rightX))
+            print('RightY= {:f}'.format(rightY))
+            os.system('clear')
+        
 
